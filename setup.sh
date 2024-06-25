83a04b6123c04b888ca67d232d4be8067ad932ae
pettijohn <632844+pettijohn@users.noreply.github.com>
2024-06-25 07:45:03 -0700

    Initial version

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 1. Install Docker https://docs.docker.com/engine/install/ubuntu/
+# Remove any old docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify
+sudo docker run hello-world
+
+
+
+
+
+# 2. Install OpenVino GPU support https://docs.openvino.ai/2024/get-started/configurations/configurations-intel-gpu.html
---
+++ verify.sh
@@ -0,0 +1,3 @@
+sudo clinfo | grep "Driver Version"
+
+sudo docker run -it --rm --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev samples/cpp/samples_bin/samples_bin/hello_query_device
