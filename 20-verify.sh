8d926cfd95bdf10012ed328ea653e02e5beb95fd
pettijohn <632844+pettijohn@users.noreply.github.com>
2024-06-26 08:47:02 -0700

    Env variable bugfix

--- 20-verify.sh
+++ 20-verify.sh
@@ -7,4 +7,6 @@ hwinfo --display
 
 sudo clinfo | grep "Driver Version"
 
-sudo docker run -it --rm --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev samples/cpp/samples_bin/samples_bin/hello_query_device
+# Need env variables to work around 6.8 kernel support issue https://github.com/intel/compute-runtime/issues/710
+#sudo docker run -it --rm --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev samples/cpp/samples_bin/samples_bin/hello_query_device
+docker run -it --rm --env NEOReadDebugKeys=1 --env OverrideGpuAddressSpace=48 --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev samples/cpp/samples_bin/samples_bin/hello_query_device
