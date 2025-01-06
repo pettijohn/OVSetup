550d7566d86dcf6ba45b5d87272c0990d6e5499d
pettijohn <632844+pettijohn@users.noreply.github.com>
2025-01-06 19:17:12 +0000

    Using Ubuntu 24 base image for verify

--- 20-verify.sh
+++ 20-verify.sh
@@ -11,7 +11,9 @@ sudo clinfo | grep "Driver Version"
 # Need env variables to work around 6.8 kernel support issue https://github.com/intel/compute-runtime/issues/710
 #sudo docker run -it --rm --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev samples/cpp/samples_bin/samples_bin/hello_query_device
 #docker run -it --rm --env NEOReadDebugKeys=1 --env OverrideGpuAddressSpace=48 --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev samples/cpp/samples_bin/samples_bin/hello_query_device
-docker run -it --rm --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev:2024.4.0 samples/cpp/samples_bin/samples_bin/hello_query_device
 
 #docker run -d --name openvino --volume=.:/Code --env NEOReadDebugKeys=1 --env OverrideGpuAddressSpace=48 --device /dev/dri/ --group-add="$(stat -c "%g" /dev/dri/renderD128)" openvino/ubuntu22_dev 
 #docker exec openvino bash
+
+docker run -it --rm --device /dev/dri/renderD128 --group-add="$(stat -c "%g" /dev/dri/renderD128)" --entrypoint /opt/intel/openvino_2024.6.0.0/samples/cpp/samples_bin/samples_bin/hello_query_device openvino/ubuntu24_dev:latest
+
